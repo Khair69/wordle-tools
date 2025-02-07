ff7ead79120c9597dbfceb55909395487315a286
Khair Horani <khair04@outlook.com>
2025-02-07 16:28:06 +0300

    0.8

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
+#read words form txt file
 def read_words_from_file(file_path):
     with open(file_path, 'r') as file:
         words_list = [line.strip() for line in file]
     return words_list
 words_list = words_list = read_words_from_file("words.txt")
 
+#letters by frequency
 def freqOfLetters():
     res = {
         'a':0 , 'b':0 , 'c':0 , 'd':0 , 'e':0 , 'f':0 , 'g':0 , 'h':0 , 'i':0 , 'j':0 , 'k':0 , 'l':0 , 'm':0 , 'n':0 , 'o':0 , 'p':0 , 'q':0 , 'r':0 , 's':0 , 't':0 , 'u':0 , 'v':0 , 'w':0 , 'x':0 , 'y':0 , 'z':0 
@@ -15,6 +17,7 @@ def freqOfLetters():
                     res[l] += 1
     return dict(sorted(res.items(), key=lambda item: item[1], reverse=True))
 
+#find the top 3 words
 def find_three_five_letter_words(words, letters):
     from itertools import combinations
     # Check if a combination of three words uses all 15 letters
@@ -23,6 +26,7 @@ def find_three_five_letter_words(words, letters):
             return combo
     return None
 
+#input letters to find words
 def find_words_with_letters(words, letters):
     letters_set = set(letters)
     matching_words = [word for word in words if set(word) == letters_set]
@@ -35,7 +39,8 @@ Here's a bunch of tools to help you better your wordle game.
     MENU:
         1 - letters frequency
         2 - three words that include the top 15 letters
-        3 - input five letters and find every word with the combination
+        3 - enter five letters and find every word with the combination
+        4 - enter 15 letters and find 3 words that include them all
         0 - exit
 please enter a number: """)
     order = int(input())
@@ -48,7 +53,7 @@ please enter a number: """)
             letters = list("earotlisncuydhp")  # top 15 letters
             result = find_three_five_letter_words(words_list, letters)
             if result:
-                print("Three five-letter words with the top 15 letters:", result)
+                print("your words are:", result)
             else:
                 print("No combination found")
         case 3:
@@ -60,5 +65,17 @@ please enter a number: """)
                     print("your words are: ", result)
                 else:
                     print("no matching words found")
+            else:
+                print("invalid input")
+        case 4:
+            print("enter 15 letters (no spaces between them)")
+            let = input()
+            if (len(let) == 15):
+                letters = list(let)
+                result = find_three_five_letter_words(words_list, letters)
+                if result:
+                    print("your words are: ", result)
+                else:
+                    print("No combination found")
             else:
                 print("invalid input")
