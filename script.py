0e22d7950690a95a9e53f48bdae7cece69fdff25
Khair Horani <khair04@outlook.com>
2025-02-07 16:03:05 +0300

    feature

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ def freqOfLetters():
             for l in res:
                 if l == let:
                     res[l] += 1
-    print(dict(sorted(res.items(), key=lambda item: item[1], reverse=True)))
+    return dict(sorted(res.items(), key=lambda item: item[1], reverse=True))
 
 def find_three_five_letter_words(words, letters):
     from itertools import combinations
@@ -23,13 +23,42 @@ def find_three_five_letter_words(words, letters):
             return combo
     return None
 
-letters = list("earotlisncuydhp")  # top 15 letters
+def find_words_with_letters(words, letters):
+    letters_set = set(letters)
+    matching_words = [word for word in words if set(word) == letters_set]
+    return matching_words
 
-result = find_three_five_letter_words(words_list, letters)
+order = -1
+while order !=0:
+    print("""
+Here's a bunch of tools to help you better your wordle game.
+    MENU:
+        1 - letters frequency
+        2 - three words that include the top 15 letters
+        3 - input five letters and find every word with the combination
+        0 - exit
+please enter a number: """)
+    order = int(input())
 
-if result:
-    print("Three five-letter words:", result)
-else:
-    print("No combination found")
-
-freqOfLetters()
+    match(order):
+        case 1:
+            result = freqOfLetters()
+            print(result)
+        case 2:
+            letters = list("earotlisncuydhp")  # top 15 letters
+            result = find_three_five_letter_words(words_list, letters)
+            if result:
+                print("Three five-letter words with the top 15 letters:", result)
+            else:
+                print("No combination found")
+        case 3:
+            print("enter five letters (no spaces between them)")
+            let = input()
+            if (len(let) == 5):
+                result = find_words_with_letters(words_list, let)
+                if result:
+                    print("your words are: ", result)
+                else:
+                    print("no matching words found")
+            else:
+                print("invalid input")
